91c497ce336411ae8279a9a3274a3803c21cf6d9
Nikolay.Plastinin <Nikolay.Plastinin@x5.ru>
2025-01-18 00:04:39 +0800

    after review 3

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
     }),
     ],
     build: {
-        outDir: 'dist',
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
