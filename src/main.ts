b78f09ea3d0a259130a24d1a1459127aff00ff07
Nikolay.Plastinin <Nikolay.Plastinin@x5.ru>
2025-01-17 23:08:48 +0800

    after review

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
+const PAGE_NAMES = [
+    '404',
+    '500',
+    'auth',
+    'chats',
+    'profile',
+    'profile-edit',
+    'registration'
+]
 
+addEventListener("DOMContentLoaded", () => {
+    if (window.location.pathname === '/') {
+        window.location.replace("/src/pages/registration.html");
+    } else if (PAGE_NAMES.some((page) => page !== window.location.pathname) ) {
+        window.location.replace("/src/pages/404.html");
+    }
+})
